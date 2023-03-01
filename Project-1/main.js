530324d3051dba439eb91fc777dc95ec8ff27285
klp59 <122928899+klp59@users.noreply.github.com>
2023-02-28 22:46:08 -0500

    1

---
+++ Project-1/main.js
@@ -0,0 +1,94 @@
+document.body.onkeydown = function(keypress_event){
+  var key = keypress_event.key;
+  document.getElementById("key_display").innerHTML = key;
+
+  var new_element = document.createElement("h1");
+  new_element.innerHTML = key;
+  new_element.classList.add("xl-type");
+  document.body.appendChild(new_element);
+
+  if (key == "a") {
+     document.body.style.backgroundColor = "pink";
+  } else if (key == "b") {
+     document.body.style.backgroundColor = "darkorange";
+  }
+    else if (key == "c") {
+     document.body.style.backgroundColor = "magenta";
+  }
+    else if (key == "d") {
+     document.body.style.backgroundColor = "darkblue";
+  }
+    else if (key == "e") {
+     document.body.style.backgroundColor = "palegreen";
+  }
+    else if (key == "f") {
+     document.body.style.backgroundColor = "lightpurple";
+  }
+    else if (key == "g") {
+     document.body.style.backgroundColor = "yellow";
+  }
+    else if (key == "h") {
+     document.body.style.backgroundColor = "white";
+  }
+    else if (key == "i") {
+     document.body.style.backgroundColor = "black";
+  }
+    else if (key == "j") {
+     document.body.style.backgroundColor = "blue";
+  }
+    else if (key == "k") {
+     document.body.style.backgroundColor = "pink";
+  }
+    else if (key == "l") {
+     document.body.style.backgroundColor = "red";
+  }
+    else if (key == "m") {
+     document.body.style.backgroundColor = "red";
+  }
+    else if (key == "n") {v
+     document.body.style.backgroundColor = "yellow";
+  }
+    else if (key == "o") {
+     document.body.style.backgroundColor = "cyan";
+  }
+    else if (key == "p") {
+     document.body.style.backgroundColor = "yellow";
+  }
+    else if (key == "q") {
+     document.body.style.backgroundColor = "grey";
+  }
+    else if (key == "r") {
+     document.body.style.backgroundColor = "pink";
+  }
+    else if (key == "s") {
+     document.body.style.backgroundColor = "red";
+  }
+    else if (key == "t") {
+  document.body.style.backgroundColor = "magenta";
+  }
+    else if (key == "u") {
+     document.body.style.backgroundColor = "green";
+  }
+    else if (key == "v") {
+     document.body.style.backgroundColor = "blue";
+  }
+    else if (key == "w") {
+     document.body.style.backgroundColor = "hotpink";
+  }
+    else if (key == "x") {
+     document.body.style.backgroundColor = "yellowgreen";
+  }
+    else if (key == "y") {
+     document.body.style.backgroundColor = "maroon";
+  }
+    else if (key == "z") {
+     document.body.style.backgroundColor = "aqua";
+  }
+}
+
+
+// timer
+// write a function that updates the innerHTML of an element
+// use setInterval to repeatedly fire that funciton every second
+// add a button to your HTML
+// with javascript, when the button is pressed, start the setInterval
